69de82944189301b7d23e69e30caf4eab12b9f7f
S.N COMPUTERINOX <S.N COMPUTERINOX@DESKTOP-JJDFFJA>
2023-10-06 12:47:33 +0530

    Add project files.

---
+++ MvcCheckBox/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCheckBox.Models
+{
+    public class Member
+    {
+        public String Name { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
